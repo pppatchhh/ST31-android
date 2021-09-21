6174412bf74a5e415e38962fde28371331f2e264
Azu0925 <yamaneko564783@gmail.com>
2021-09-22 02:37:34 +0900

    編集画面作成まで

--- Saigoku33Memo90032/app/src/main/java/local/hal/st31/android/saigoku33memo90032/MainActivity.java
+++ Saigoku33Memo90032/app/src/main/java/local/hal/st31/android/saigoku33memo90032/MainActivity.java
@@ -3,6 +3,8 @@ package local.hal.st31.android.saigoku33memo90032;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private class ListItemClickListender implements AdapterView.onItemClickListener {
+        @override
+        public void onItemClick(AdapterView<?> parent)
     }
 }
--- Saigoku33Memo90032/app/src/main/java/local/hal/st31/android/saigoku33memo90032/SubActivity.java
+++
@@ -1,4 +0,0 @@
-package local.hal.st31.android.saigoku33memo90032;
-
-public class SubActivity {
-}
