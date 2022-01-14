e5356f03cbcf84099733e0397ec484d4647c2345
Azu0925 <yamaneko564783@gmail.com>
2022-01-15 00:19:40 +0900

    No.10のプロジェクト作成とstringsの作成

---
+++ Post2DB90032/app/src/main/java/local/hal/st31/android/post2db90032/MainActivity.java
@@ -0,0 +1,14 @@
+package local.hal.st31.android.post2db90032;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
