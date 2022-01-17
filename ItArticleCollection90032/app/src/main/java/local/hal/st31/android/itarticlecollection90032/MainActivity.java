348f55f480ab2fe143c89a7441b44e360eaca9af
Azu0925 <yamaneko564783@gmail.com>
2022-01-18 02:55:51 +0900

    json取得途中

--- ItArticleCollection90032/app/src/main/java/local/hal/st31/android/itarticlecollection90032/MainActivity.java
+++ ItArticleCollection90032/app/src/main/java/local/hal/st31/android/itarticlecollection90032/MainActivity.java
@@ -1,14 +1,134 @@
 package local.hal.st31.android.itarticlecollection90032;
 
+import androidx.annotation.UiThread;
+import androidx.annotation.WorkerThread;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.os.HandlerCompat;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.SocketTimeoutException;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String ACCESS_URL = "https://hal.architshin.com/st31/getItArticlesList.php";
+    private static final String DEBUG_LOG = "ItArticleColleection";
+    private List<Map<String, String>> _list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @UiThread
+    private void receiveArticleList(final String url){
+        Looper mainLooper = Looper.getMainLooper();
+        Handler handler = HandlerCompat.createAsync(mainLooper);
+
+    }
+
+    private class ArticleListBackgroundReceiver implements Runnable {
+        private final Handler _handler;
+        private final String _url;
+
+        public ArticleListBackgroundReceiver(Handler handler, String url){
+            _handler = handler;
+            _url = url;
+        }
+
+        @WorkerThread
+        @Override
+        public void run(){
+            HttpURLConnection con = null;
+            InputStream is = null;
+            String result = "";
+            try{
+                URL url = new URL(_url);
+                con = (HttpURLConnection) url.openConnection();
+                con.setConnectTimeout(1000);
+                con.setReadTimeout(1000);
+                con.setRequestMethod("GET");
+                con.connect();
+                int status = con.getResponseCode();
+                if(status != 200){
+                    throw new IOException();
+                }
+                is = con.getInputStream();
+                result = is2String(is);
+            }
+            catch(MalformedURLException ex){
+                Log.e(DEBUG_LOG, "URL変換失敗", ex);
+            }
+            catch(SocketTimeoutException ex){
+                Log.e(DEBUG_LOG, "通信タイムアウト", ex);
+            }
+            catch(IOException ex){
+                Log.e(DEBUG_LOG, "通信失敗", ex);
+            }
+            finally {
+                if(con != null){
+                    con.disconnect();
+                }
+                if(is != null){
+                    try {
+                        is.close();
+                    }
+                    catch (IOException ex){
+                        Log.e(DEBUG_LOG, "InputStream解放失敗", ex);
+                    }
+                }
+            }
+        }
+
+        private String is2String(InputStream is) throws IOException{
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+            StringBuffer sb = new StringBuffer();
+            char[] b = new char[1024];
+            int line;
+            while(0 <= (line = reader.read(b))){
+                sb.append(b, 0, line);
+            }
+            return sb.toString();
+        }
+    }
+
+    private class ArticleListPostExecutor implements Runnable{
+        private final String _result;
+
+        public ArticleListPostExecutor(String result){
+            _result = result;
+        }
+
+        @UiThread
+        @Override
+        public void run(){
+            String status = "";
+            String msg = "";
+            try {
+                JSONObject rootJSON = new JSONObject(_result);
+                status = rootJSON.getString("status");
+                msg = rootJSON.getString("msg");
+
+            }
+            catch (JSONException ex){
+                Log.e(DEBUG_LOG, "JSON解析失敗", ex);
+            }
+
+        }
     }
 }
