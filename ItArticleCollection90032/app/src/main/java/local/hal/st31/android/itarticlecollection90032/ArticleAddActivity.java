db080b7a6df97e4b2f9915c0600cfe6fb6eadcfa
Azu0925 <yamaneko564783@gmail.com>
2022-01-18 19:20:32 +0900

    No.11 アクティビティ作成

---
+++ ItArticleCollection90032/app/src/main/java/local/hal/st31/android/itarticlecollection90032/ArticleAddActivity.java
@@ -0,0 +1,14 @@
+package local.hal.st31.android.itarticlecollection90032;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ArticleAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_article_add);
+    }
+}
